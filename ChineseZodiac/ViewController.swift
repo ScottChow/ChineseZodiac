0069a2dd61101aa7603a0d4b140edeff496a45b5
ScottChow <ScottChow@live.cn>
2015-12-28 18:39:11 +0800

    new

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loveYou: UILabel!
+    
+    @IBOutlet weak var yearOfBirth: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        yearOfBirth.resignFirstResponder()
+    }
 
+    @IBAction func okTapped(sender: AnyObject) {
+        yearOfBirth.resignFirstResponder()
+        if let year = Int(yearOfBirth.text!){
+            let imageNumber = (year-4)%12
+            image.image = UIImage(named:String(imageNumber))
+            
+            
+        }
+        let year1 = Int(yearOfBirth.text!)
+        if year1 == 1989{
+            loveYou.text = "蛋殼我愛妳"
+        }
+        
+        
+    }
 
 }
 
